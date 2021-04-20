d704159c6759d8861fade7d43a06ace0875a1107
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-04-20 23:23:53 +0600

    script for to change the scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIdx = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIdx + 1);
+    }
+}
